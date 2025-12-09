f929f28ab0b7e1170497a5f3f10daa511c003158
Czarek Nakamoto <cyjan@mrcyjanek.net>
2025-12-09 12:02:04 +0100

    wip

--- build.sh
+++ build.sh
@@ -33,16 +33,21 @@ go run . -cleanup
 
 for target in "$@";
 do
+    SO_PATH=.buildlib/env
+    if [[ ! "x$SIMPLYBS_ENV_DIR" == "x" ]];
+    then
+        SO_PATH=$SIMPLYBS_ENV_DIR
+    fi
     go run . -host "$target" -extract -package torch
     if [[ "$target" == "aarch64-linux-android" ]];
     then
-        cp .buildlib/env/aarch64-linux-android/lib/libtorch.so ../android/src/main/jniLibs/arm64-v8a/libtorch.so
+        cp $SO_PATH/aarch64-linux-android/lib/libtorch.so ../android/src/main/jniLibs/arm64-v8a/libtorch.so
     elif [[ "$target" == "x86_64-linux-android" ]];
     then
-        cp .buildlib/env/x86_64-linux-android/lib/libtorch.so ../android/src/main/jniLibs/x86_64/libtorch.so
+        cp $SO_PATH/x86_64-linux-android/lib/libtorch.so ../android/src/main/jniLibs/x86_64/libtorch.so
     elif [[ "$target" == "armv7a-linux-androideabi" ]];
     then
-        cp .buildlib/env/armv7a-linux-androideabi/lib/libtorch.so ../android/src/main/jniLibs/armeabi-v7a/libtorch.so
+        cp $SO_PATH/armv7a-linux-androideabi/lib/libtorch.so ../android/src/main/jniLibs/armeabi-v7a/libtorch.so
     fi
 done
 
